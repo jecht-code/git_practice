5a000abedc27a65d639d5a1bc9dcb28c06ffd2a8
Scott Pak <gpak@saintpeters.edu>
2020-05-14 22:45:29 -0400

    push

--- datacamp.py
+++ datacamp.py
@@ -1,1 +1,20 @@
 print("hi simon!")
+
+def add_all(*args):
+    """sum all args together"""
+    #intialize sum
+    sum_all = 0
+
+    for num in args:
+        sum_all += num
+    
+    return sum_all
+
+print(add_all(1,2))
+
+def print_all(**kwargs):
+
+    for key, value in kwargs.items():
+        print(key + ":" + value)
+
+print_all(name='dumbledore', job='headmaster')          

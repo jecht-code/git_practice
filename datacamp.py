5560fcd7980c5d671ad419ecea6719b7fcc4920a
Scott Pak <gpak@saintpeters.edu>
2020-05-17 17:39:51 -0400

    git commit

--- datacamp.py
+++ datacamp.py
@@ -17,4 +17,28 @@ def print_all(**kwargs):
     for key, value in kwargs.items():
         print(key + ":" + value)
 
-print_all(name='dumbledore', job='headmaster')          
+print_all(name='dumbledore', job='headmaster')
+
+raisetopower = lambda x, y: x **y
+
+a = raisetopower(2,3)
+print(a)
+
+### Iterating over iterables
+word = 'Da'
+it = iter(word)
+print(next(it))
+print(next(it))
+
+word = "Data"
+it = iter(word)
+print(*it)
+
+print(10 ** 100)
+
+googol = iter(range(10 ** 100))
+print(next(googol))
+print(next(googol))
+
+values = range(10, 21)
+print(values)

45b06adce6128644b4015555bffd7d105a0d1e3b
Scott Pak <gpak@saintpeters.edu>
2020-08-30 00:46:42 -0400

    Added API Finance Stock
    testing...

--- README.txt
+++ README.txt
@@ -1,1 +1,3 @@
 Hello Git and Github
+## subheaders
+Watch tutorials on youtube.
---
+++ api_finance_sp.py
@@ -0,0 +1,56 @@
+##api_finance RAPID API site. Freemium 500 calls a month.
+import seaborn as sns
+import matplotlib.pyplot as plt
+import pandas as pd
+from datetime import datetime
+import requests
+from matplotlib import rcParams
+#json is part the anaconda root enviornment. no need to download. 
+import json
+
+# Check the authentication and connection
+url = "https://apidojo-yahoo-finance-v1.p.rapidapi.com/market/get-charts"
+
+querystring = {"symbol":"AAPL"}
+
+headers = {
+    'x-rapidapi-host': "apidojo-yahoo-finance-v1.p.rapidapi.com",
+    'x-rapidapi-key': "25eb8aceadmshfb3c2de6919d91fp1dfb74jsnc41ea485775c"
+    }
+
+response = requests.request("GET", url, headers=headers, params=querystring)
+#status code for when checking to see if you get a correct response.
+print(response.status_code)
+
+parsedata = response.json()
+print(len(parsedata))
+parse_str = json.dumps(parsedata['chart']['result'][0], indent=2)
+print(parse_str)
+
+time_stamp = parsedata['chart']['result'][0]['timestamp']
+
+calendartime= []
+for ts in time_stamp:
+    dt = datetime.fromtimestamp(ts)
+    calendartime.append(dt.strftime("%m/%d/%Y %r"))
+print(calendartime)
+
+stock_low = parsedata["chart"]["result"][0]["indicators"]["quote"][0]["low"]
+stock_close = parsedata["chart"]["result"][0]["indicators"]["quote"][0]["close"]
+stock_open = parsedata["chart"]["result"][0]["indicators"]["quote"][0]["open"]
+stock_high = parsedata["chart"]["result"][0]["indicators"]["quote"][0]["high"]
+stock_volume = parsedata["chart"]["result"][0]["indicators"]["quote"][0]["volume"]
+
+my_dict = {'Time': calendartime,
+'Low': stock_low,
+'Close': stock_close,
+'Open' : stock_open,
+'High' : stock_high,
+'Volume': stock_volume
+}
+
+all_info = pd.DataFrame(my_dict)
+
+print(all_info)
+#print(response.body)
+#print(response.text)
